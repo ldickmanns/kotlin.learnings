f1777cecefcbf1528931a27c74e96c5fe860002b
Ludwig Dickmanns <ludwig.dickmanns@outlook.de>
2019-11-28 09:09:17 +0100

    IfThenElse and When post.

---
+++ src/basics/IfThenElse.kt
@@ -0,0 +1,32 @@
+package basics
+
+/*********************
+ * Kotlin Learnings  *
+ * @kotlin.learnings *
+ *********************/
+
+fun main() {
+    val a = 2
+    val b = 3
+
+    // One line if
+    var max = a
+    if (a < b) max = b
+    println(max)
+
+    // Multi line if with else
+    var min: Int
+    if (a < b) {
+        min = a
+    } else {
+        min = b
+    }
+    println(min)
+}
+
+/************
+ * Output:  *
+ * 3        *
+ * 2        *
+ ************/
+
