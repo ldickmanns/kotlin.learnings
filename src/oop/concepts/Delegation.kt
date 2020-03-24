b7b281300d6563c573f6d3ac9a8652e235a39296
Ludwig Dickmanns <ludwig.dickmanns@outlook.de>
2020-03-24 12:06:46 +0100

    Added file for delegation.

---
+++ src/oop/concepts/Delegation.kt
@@ -0,0 +1,37 @@
+package oop.concepts
+
+/************************
+ * Kotlin Learnings     *
+ * @kotlin.learnings    *
+ ************************/
+
+fun main() {
+    val bus = BusImpl()
+    val taxi = BusTaxi(bus)
+    taxi.drive()
+    taxi.pickPeopleUp()
+    taxi.drive()
+    taxi.dropPeople()
+}
+
+interface Bus {
+    fun drive()
+}
+
+class BusImpl : Bus {
+    override fun drive() = println("Diving")
+}
+
+class BusTaxi(bus: Bus) : Bus by bus {
+    fun pickPeopleUp() = println("Picking up people")
+    fun dropPeople() = println("Dropping people")
+}
+
+/************************
+ * Output:              *
+ * Diving               *
+ * Picking up people    *
+ * Diving               *
+ * Dropping people      *
+ ************************/
+
