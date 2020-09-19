d87535a0f5758535cb3571cc3b245eae0258f7d6
Ludwig Dickmanns <ludwig.dickmanns@outlook.de>
2020-09-19 12:18:43 +0200

    Post 33: Kotlin vs. Java. Casting.

---
+++ src/kotlinVsJava/casting/CastingInJava.java
@@ -0,0 +1,30 @@
+package kotlinVsJava.casting;
+
+/********************
+ * Kotlin Learnings   *
+ * @kotlin.learnings  *
+ ********************/
+
+public class CastingInJava {
+    public static void main(String[] args) {
+        /** Implicit cast */
+        int i = 3;
+        long l = i;
+
+        /** Explicit cast */
+        class Building {}
+        class House extends Building {}
+        Building building = new House();
+        House house = (House)building;
+
+        System.out.println(i);
+        System.out.println(l);
+        System.out.println(house);
+    }
+}
+
+/****************
+ * Output:      *
+ *              *
+ ****************/
+
---
+++ src/kotlinVsJava/casting/CastingInKotlin.kt
@@ -0,0 +1,28 @@
+package kotlinVsJava.casting
+
+/************************
+ * Kotlin Learnings     *
+ * @kotlin.learnings    *
+ ************************/
+
+fun main() {
+    /** Widening */
+    val i = 3
+    val l = i.toLong()
+
+    /** Explicit cast */
+    open class Building
+    class House: Building()
+    val building: Building = House()
+    val house = building as House
+
+    println(i)
+    println(l)
+    println(house)
+}
+
+/****************
+ * Output:      *
+ *              *
+ ****************/
+
