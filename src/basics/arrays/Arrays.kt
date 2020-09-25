17132e17c5087c9669aefcc68231a95fc86644bd
Ludwig Dickmanns <ludwig.dickmanns@outlook.de>
2020-09-25 18:27:07 +0200

    Post 034: Arrays.

---
+++ src/basics/arrays/Arrays.kt
@@ -0,0 +1,18 @@
+package basics.arrays
+
+/************************
+ * Kotlin Learnings     *
+ * @kotlin.learnings    *
+ ************************/
+
+fun main() {
+    // Array factory function
+    val array = arrayOf("Hello World!", 42)
+    println(array)
+}
+
+/****************
+ * Output:      *
+ *              *
+ ****************/
+
---
+++ src/basics/arrays/Constructor.kt
@@ -0,0 +1,19 @@
+package basics.arrays
+
+/************************
+ * Kotlin Learnings     *
+ * @kotlin.learnings    *
+ ************************/
+
+fun main() {
+    // Constructor
+    val array = Array(5) { it }
+    array.forEach { print(it) }
+    // Output: 01234
+}
+
+/****************
+ * Output:      *
+ *              *
+ ****************/
+
