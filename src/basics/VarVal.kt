c224b66b8b882f65dc7dfb092a27be771bdc44ef
Ludwig Dickmanns <ludwig.dickmanns@outlook.de>
2021-04-02 12:59:27 +0200

    Adjusted folder structure for conditionals.

---
+++ src/basics/VarVal.kt
@@ -0,0 +1,17 @@
+package basics
+
+/************************
+ * Kotlin Learnings     *
+ * @kotlin.learnings    *
+ ************************/
+
+fun main() {
+    val i = 5  // This is immutable
+    var j = 5  // This is mutable
+}
+
+/****************
+ * Output:      *
+ *              *
+ ****************/
+
