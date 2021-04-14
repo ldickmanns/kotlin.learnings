f00852e555f918889a4e892a5bd90d0428636803
Ludwig Dickmanns <ludwig.dickmanns@outlook.de>
2021-04-15 00:04:57 +0200

    Added code snippet for basics.basicSyntax.Collections.

---
+++ src/docs/basics/basicSyntax/Collections.kt
@@ -0,0 +1,33 @@
+package docs.basics.basicSyntax
+
+/************************
+ * Kotlin Learnings     *
+ * @kotlin.learnings    *
+ ************************
+ * Collections          *
+ ************************/
+
+fun main() {
+    val numbers = listOf(1, 2, 3, 4, 5)
+
+    /** Iterating over a collection */
+    for (number in numbers) {
+        print(number)
+    } /** Output: 12345 */
+    println()
+
+    /** Check if a collection contains something */
+    when {
+        6 in numbers -> println("Six is in the list")
+        6 !in numbers -> println("Six isn't in the list")
+    } /** Output: Six isn't in the list */
+
+    /** Lambdas x Collections */
+    val animals = listOf("Dog", "Cat", "Dolphin", "Duck")
+    animals
+        .filter { it.startsWith("D") }
+        .sortedBy { it }
+        .map { it.toLowerCase() }
+        .forEach { print("$it ") }
+    /** Output: dog dolphin duck*/
+}
