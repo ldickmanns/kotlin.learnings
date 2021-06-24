3b526d53d0e89f64ded9d62bd74a9525d959bd86
ldickmanns <ludwig.dickmanns@outlook.de>
2021-06-24 17:51:54 +0200

    Corrected code for doc.concepts.controlFlow.labels.LabeledReturn.

---
+++ src/docs/concepts/controlFlow/labels/LabeledReturn.kt
@@ -0,0 +1,33 @@
+package docs.concepts.controlFlow.labels
+
+/********************
+ * Kotlin Learnings *
+ ********************
+ * Labeled Return   *
+ ********************/
+
+fun normalReturn() {
+    listOf("Car", "Ball", "Cat").forEach {
+        if (it == "Ball") return
+        println(it)
+    }
+    println("Unreachable")
+}
+
+fun labeledReturn() {
+    listOf("Car", "Ball", "Cat").forEach here@{
+        if (it == "Ball") return@here
+        println("Car")
+    }
+    println("Reachable")
+}
+
+fun main() {
+    normalReturn()
+    // Output: Car
+
+    labeledReturn()
+    // Output:
+    // Car
+    // Reachable
+}
