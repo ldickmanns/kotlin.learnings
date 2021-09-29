15f96d534243cb370cdae83ce3d52f91186cd3de
ldickmanns <ludwig.dickmanns@outlook.de>
2021-09-29 17:18:21 +0200

    Added code for doc.concepts.controlFlow.exceptions.Exceptions.

---
+++ src/docs/concepts/controlFlow/exceptions/Exceptions.kt
@@ -0,0 +1,24 @@
+package docs.concepts.controlFlow.exceptions
+
+/********************
+ * Kotlin Learnings *
+ ********************
+ * Exceptions       *
+ ********************/
+
+/**
+ * You throw exceptions with the "throw" keyword. Also, functions that never return
+ * - that is, functions throwing an exception - have "Nothing" as their return type.
+ */
+fun throwException(): Nothing {
+    throw Exception()
+}
+
+fun main() {
+    throwException()
+
+    // Output:
+    // Exception in thread "main" java.lang.Exception: This is an exception
+    //     at docs.concepts.controlFlow.exceptions.ExceptionsKt.main(Exceptions.kt:11)
+    //     at docs.concepts.controlFlow.exceptions.ExceptionsKt.main(Exceptions.kt)
+}
