97a85690633ec99cb28ff2bf47d2e5b75425eb0c
ldickmanns <ludwig.dickmanns@outlook.de>
2021-09-29 17:18:28 +0200

    Added code for doc.concepts.controlFlow.exceptions.TryCatch.

---
+++ src/docs/concepts/controlFlow/exceptions/TryCatch.kt
@@ -0,0 +1,23 @@
+package docs.concepts.controlFlow.exceptions
+
+/***********************
+ * Kotlin Learnings    *
+ ***********************
+ * Try, catch, finally *
+ ***********************/
+
+/**
+ * Try, catch, and finally work as you know
+ * them from most other programming languages.
+ */
+fun main() {
+    try {
+        // Try something.
+    } catch (e: Exception) {
+        // Zero or more catch blocks
+        // for different exceptions.
+    } finally {
+        // finally is optional. The code
+        // here is always executed.
+    }
+}
