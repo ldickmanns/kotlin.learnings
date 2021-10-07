e151e3898bc36f79d2d40d38838ea4e6fd8a4d9c
ldickmanns <ludwig.dickmanns@outlook.de>
2021-10-07 09:18:38 +0200

    Added code for doc.concepts.classesAndObjects.Classes.

---
+++ src/docs/concepts/classesAndObjects/Classes.kt
@@ -0,0 +1,18 @@
+package docs.concepts.classesAndObjects
+
+/********************
+ * Kotlin Learnings *
+ ********************
+ * Classes          *
+ ********************/
+
+/** Classes are defined with the class
+ *  keyword and can be empty */
+class EmptyClass
+
+fun main() {
+    val ec = EmptyClass()
+
+    println(ec::class.simpleName)
+    // Output: EmptyClass
+}
