9b07451852fafef757e1ce35ed6a0e452ded0b39
ldickmanns <ludwig.dickmanns@outlook.de>
2022-02-04 08:34:53 +0100

    Added code for docs.destructuringDeclarations.Pairs.

---
+++ src/docs/destructuringDeclarations/Pairs.kt
@@ -0,0 +1,15 @@
+package docs.destructuringDeclarations
+
+/********************
+ * Kotlin Learnings *
+ ********************
+ * Pairs            *
+ ********************/
+
+fun main() {
+    val laptop = Pair("MacBook", 1000)
+    /** This is called a destructuring declaration. */
+    val (name, price) = laptop
+    println("$name costs $price$")
+    // Output: MacBook costs 1000$
+}
