482db5f25055369bf3869e64f6d857dbca7d634b
ldickmanns <ludwig.dickmanns@outlook.de>
2022-02-04 08:46:05 +0100

    Added code for docs.destructuringDeclarations.Maps.

---
+++ src/docs/destructuringDeclarations/Maps.kt
@@ -0,0 +1,23 @@
+package docs.destructuringDeclarations
+
+/********************
+ * Kotlin Learnings *
+ ********************
+ * Maps             *
+ ********************/
+
+fun main() {
+    val map = mapOf(
+        "Mike" to "Car",
+        "Luca" to "Motorcycle",
+        "Liam" to "Bicycle",
+    )
+    /** Conveniently traverse a map. */
+    for ((person, vehicle) in map) {
+        println("$person is driving by $vehicle.")
+    }
+    // Output:
+    // Mike is driving by Car.
+    // Luca is driving by Motorcycle.
+    // Liam is driving by Bicycle.
+}
