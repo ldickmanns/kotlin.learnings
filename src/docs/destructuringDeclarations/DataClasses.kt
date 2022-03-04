8416d957531a32581e4d424f4af28a091ba9a2eb
ldickmanns <ludwig.dickmanns@outlook.de>
2022-03-04 08:38:12 +0100

    Added code for docs.destructuringDeclarations.DataClasses.

---
+++ src/docs/destructuringDeclarations/DataClasses.kt
@@ -0,0 +1,23 @@
+package docs.destructuringDeclarations
+
+/********************
+ * Kotlin Learnings *
+ ********************
+ * Data Classes     *
+ ********************/
+
+data class Person(
+    val name: String,
+    val age: Int,
+)
+
+fun main() {
+    val me = Person("Ludwig", 26)
+    /**
+     * Destructuring can be used to access the
+     *
+     * individual components of a data class
+     */
+    val (myName, myAge) = me
+    println("My name is $myName and I'm $myAge")
+}
