a7628d6eebcb66c48998e1018dcd82ccb65bf947
ldickmanns <ludwig.dickmanns@outlook.de>
2022-03-07 19:06:50 +0100

    Adjusted code for docs.concepts.classesAndObjects.dataClasses.DataClasses.

--- src/docs/concepts/classesAndObjects/dataClasses/DataClasses.kt
+++ src/docs/concepts/classesAndObjects/dataClasses/DataClasses.kt
@@ -6,20 +6,20 @@ package docs.concepts.classesAndObjects.dataClasses
  * Data Classes     *
  ********************/
 
-/** Use the **`data`** keyword to declare a data class: */
+/** Use the `data` keyword to declare a data class: */
 data class Person(val name: String)
 
 fun main() {
     val alice = Person("Alice")
     val bob = Person("Bob")
 
-    /** The compiler automatically derives an **`equals`** function: */
+    /** The compiler automatically derives an `equals` function: */
     val aliceEqualsBob = alice == bob
     println(aliceEqualsBob) // false
     val secondAlice = Person("Alice")
     println(alice == secondAlice) // true
 
-    /** Further, you get a **`toString`** function: */
+    /** Further, you get a `toString` function: */
     val ludwig = Person("Ludwig")
     println("Hey guys, I'm $ludwig")
     // Hey guys, I'm Person(name=Ludwig)
