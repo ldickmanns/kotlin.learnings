58006dae6964b31146380c979829603db352a6f6
ldickmanns <ludwig.dickmanns@outlook.de>
2022-03-17 12:53:58 +0100

    Updated gradle dependency versions.

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,8 +18,8 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.10")
     implementation("org.openjfx:javafx-fxml:17.0.2")
 
-    implementation("org.slf4j:slf4j-api:1.7.33")
-    implementation("org.slf4j:slf4j-simple:1.7.33")
+    implementation("org.slf4j:slf4j-api:1.7.36")
+    implementation("org.slf4j:slf4j-simple:1.7.36")
     implementation("org.jetbrains.kotlinx:kotlin-deeplearning-api:0.3.0")
 
 //    compile 'junit:junit:4.12'
