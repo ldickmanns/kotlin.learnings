4bbe98cb5d116126d5586c953c4fb58d92316b45
ldickmanns <ludwig.dickmanns@outlook.de>
2022-07-19 11:03:02 +0200

    Corrected listOfNotNull post.

--- src/learnings/kotlin/dataStructures/lists/NotNull.kt
+++ src/learnings/kotlin/dataStructures/lists/NotNull.kt
@@ -9,10 +9,19 @@ fun main() {
     val alice = User("Alice", 21)
     val moviesForAlice = getRecommendations(alice)
     println(moviesForAlice)
+    // [Comedy Movie, Documentation, Action Movie,
+    // Horror Movie, Love Story Movie]
 
     val bob = User("Bob", 17)
     val moviesForBob = getRecommendations(bob)
     println(moviesForBob)
+    // [Comedy Movie, Documentation,
+    // Action Movie, Love Story Movie]
+
+    val charlie = User("Charlie", 14)
+    val moviesForCharlie = getRecommendations(charlie)
+    println(moviesForCharlie)
+    // [Comedy Movie, Documentation, Love Story Movie]
 }
 
 fun getRecommendations_(user: User): List<String> {
@@ -20,8 +29,8 @@ fun getRecommendations_(user: User): List<String> {
     movies.add("Comedy")
     movies.add("Documentation")
     if (user.age >= 16) movies.add("Action")
-    if (user.age >= 18) movies.add("HorrorD")
-    movies.add("Love Story Movie")
+    if (user.age >= 18) movies.add("Horror")
+    movies.add("Love Story")
     return movies
 }
 
@@ -31,14 +40,14 @@ fun getRecommendations__(
     add("Comedy")
     add("Documentation")
     if (user.age >= 16) add("Action")
-    if (user.age >= 18) add("HorrorD")
-    add("Love Story Movie")
+    if (user.age >= 18) add("Horror")
+    add("Love Story")
 }.toList()
 
 fun getRecommendations(user: User) = listOfNotNull(
-    "Comedy Movie",
+    "Comedy",
     "Documentation",
-    "Action Movie".takeIf { user.age >= 16 },
-    "Horror Movie".takeIf { user.age >= 18 },
-    "Love Story Movie",
+    "Action".takeIf { user.age >= 16 },
+    "Horror".takeIf { user.age >= 18 },
+    "Love Story",
 )
