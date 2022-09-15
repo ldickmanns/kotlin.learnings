bdd05f8fbf526f5ff5989cc1b05cbed7de370007
ldickmanns <ludwig.dickmanns@outlook.de>
2022-09-15 12:40:47 +0200

    Added code for the second quiz.

---
+++ src/quizzes/Quiz2.kt
@@ -0,0 +1,28 @@
+package quizzes
+
+fun main() {
+    for (i in Test()) println(i)
+
+    for (j in Test2()) println(j)
+
+    for (k in Test3()) println(k)
+}
+
+val iterator: Iterator<Int>
+    get() = object : Iterator<Int> {
+        var hasNext = true
+        override fun hasNext(): Boolean = hasNext.also { hasNext = false }
+
+        override fun next(): Int = 1
+    }
+
+class Test : Iterable<Int> {
+    override fun iterator(): Iterator<Int> = iterator
+}
+
+class Test2 : Iterator<Int> by iterator
+
+
+class Test3 {
+    operator fun iterator(): Iterator<Int> = iterator
+}
