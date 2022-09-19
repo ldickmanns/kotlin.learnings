88bb0e0acb4f6ce0dc71fd4c8d71835c6afdf83a
ldickmanns <ludwig.dickmanns@outlook.de>
2022-09-19 12:57:30 +0200

    Added code for the third quiz.

---
+++ src/quizzes/Quiz3.kt
@@ -0,0 +1,16 @@
+package quizzes
+
+/********************
+ * Kotlin Learnings *
+ ********************
+ * Quiz 3           *
+ ********************/
+
+fun main() {
+    println("" == "")
+    println("" === "")
+    println(String() == "")
+    println(String() === "")
+    println(String() == String())
+    println(String() === String())
+}
